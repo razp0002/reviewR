ffd578a2076ce2ffdc71c505f925acf6d4569fe2
razp0002 <razp0002@algonquinlive.com>
2016-04-07 11:54:53 -0400

    final commit

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
 var app = {
     METHOD: "POST",
-    UUID: "stan",
     init: function (ev) {
-        //called after DOMContentLoaded/device ready
+        app.getList();
+        
         document.querySelector(".btnAdd").addEventListener("click", app.navigate);
-        document.querySelector("#save").addEventListener("click", function(ev){
+        document.querySelector("#save").addEventListener("click", function (ev) {
             app.saveData(ev);
             app.navigate(ev);
         });
         document.querySelector("#back").addEventListener("click", app.navigate);
-//        document.querySelector(".toDetails").addEventListener("click", app.navigate);
+        document.querySelector("#home").addEventListener("click", app.navigate);
+        document.querySelector("#reviews").addEventListener("click", function(ev){
+            app.navigate(ev);
+            app.getReview(ev);
+        });
 
         window.addEventListener("popstate", app.popPop);
-        //get list of things for the homepage 
-        app.getList();
+    },
+    useTouch: function(){
+        var touch = document.getElementById();
+        var hammertime = new Hammer(touch);
+        hammertime.on('tap', function(ev) {
+            console.log(ev);
+        });
     },
     getList: function () {
         var xhr = new XMLHttpRequest();
@@ -21,14 +30,14 @@ var app = {
         //xhr.open("POST", "get-reviews.php");
         xhr.addEventListener("load", app.gotList);
         xhr.addEventListener("error", app.badStuffHappened);
-        
+
         var params = new FormData();
-        
-        params.append("uuid", "stan");
-        
+
+        params.append("uuid", "4242");
+
         xhr.send(params);
     },
-    
+
     gotList: function (ev) {
         //when the list comes back from the server 
         var data = JSON.parse(ev.target.responseText);
@@ -43,48 +52,56 @@ var app = {
                     var li = document.createElement("li");
                     li.setAttribute("data-href", "details");
                     li.setAttribute("id", data.reviews[i].id);
-                    li.setAttribute("class", "toDetails");
-                    li.textContent = data.reviews[i].title + " \nReview: " + data.reviews[i].rating + " stars";
+                    li.setAttribute("data-id", data.reviews[i].id);
+                    li.textContent = data.reviews[i].title + " \nReview: " + data.reviews[i].rating + "/5";
                     ul.appendChild(li);
                 }
                 //forEach version
                 data.reviews.forEach(function (item, index) {
-                    var li = document.createElement("li");
-                    li.textContent = item.title;
-                    ul.appendChild(li);
+//                    var li = document.createElement("li");
+//                    li.textContent = item.title;
+//                    ul.appendChild(li);
                 });
             } else {
                 msg.innerHTML = "no reviews for you";
             }
 
-        }
-        else {
+        } else {
             //bad things happend
             // use the same function over and overafor any warning 
             console.log("NO WORK");
         }
     },
-    //    getReview : function () {
-//      
-//          var xhr = new XMLHttpRequest();
-//        xhr.open("POST", "https://griffis.edumedia.ca/mad9022/reviewr/review/get/");
-//        //xhr.open("POST", "get-reviews.php");
-//        xhr.addEventListener("load", app.gotList);
-//        xhr.addEventListener("error", app.badStuffHappened);
-//        
-//        var params = new FormData();
-//        
-//                params.append("id", id);
-//
-//        params.append("uuid", "4242");
-//        
-//        document.querySelector("#reviews").textContent = "request Sent.";
-//        
-//        xhr.send(params);
-//        
-//        
-//        
-//    },
+    getReview: function (id) {
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "https://griffis.edumedia.ca/mad9022/reviewr/review/get/");
+        xhr.addEventListener("load", app.gotReview(id));
+        xhr.addEventListener("error", app.badStuffHappened);
+
+        var params = new FormData();
+
+        params.append("id", id);
+
+        params.append("uuid", "4242");
+
+        document.querySelector("#reviews").textContent = "request Sent.";
+
+        xhr.send(params);
+    },
+    gotReview: function(){
+        var data = JSON.parse(ev.target.responseText);
+        var makeTitle = document.createElement("h2");
+        var par = document.createElement("p");
+        var par2 = document.createElement("p");
+        var pic = document.createElement("img");
+        
+        makeTitle.textContent = data.review_details.title;
+        par.textContent = "Rating: " + data.review_details.rating + "/5";
+        par2.textContent = data.review_details.review_txt;
+        img.setAttribute("src", data.review_details.img);
+        
+    },
     badStuffHappened: function (ev) {
         //this gets called for actual errors
         document.querySelector("#reviews").textContent = "ERROR " + ev.message;
@@ -98,7 +115,7 @@ var app = {
         xhr.open("POST", "https://griffis.edumedia.ca/mad9022/reviewr/review/set/");
         xhr.addEventListener("error", app.badStuffHappened);
         var params = new FormData();
-        params.append("uuid", "stan");
+        params.append("uuid", "4242");
         params.append("action", "insert");
         params.append("title", title.value);
         params.append("review_txt", review.value);
@@ -126,5 +143,4 @@ var app = {
     }
 };
 
-document.addEventListener("DOMContentLoaded", app.init);
-///deviceready
+document.addEventListener("deviceready", app.init);
